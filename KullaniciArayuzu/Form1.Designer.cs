704fd1eb8c512bf5f1c748b3876b0fa979297b2e
batin <batinayyildiz177@gmail.com>
2023-09-20 17:30:12 +0300

    Bu proje, kullanıcıların belirledikleri kaynak klasördeki fotoğrafları sıkıştıran ve her birine özel bir QR kod ekleyen bir C# uygulamasını içerir.
    Bu proje, kullanıcıların belirledikleri kaynak klasördeki fotoğrafları sıkıştıran ve her birine özel bir QR kod ekleyen bir C# uygulamasını içerir. Fotoğrafları istenen boyutlara sıkıştırırken aynı zamanda her birine benzersiz bir QR kod ekleyerek kullanıcıların fotoğrafları hızlı bir şekilde tanımlamalarına ve paylaşmalarına olanak tanır. Fotoğrafları hedef klasöre kaydederken işlem ilerlemesini izlemek için bir ilerleme çubuğu sağlar.
    
    Bu projenin ana hedefleri şunlardır:
    
    Fotoğraf sıkıştırma işlemini otomatikleştirmek ve hızlandırmak.
    Her fotoğrafa kullanıcı tarafından tanımlanmış bir QR kod eklemek.
    Kullanıcı dostu bir arayüz sağlamak.
    Bu proje, fotoğraf düzenlemeyi ve paylaşmayı kolaylaştırmak isteyenler için idealdir.

---
+++ KullaniciArayuzu/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace KullaniciArayuzu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Target";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(179, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 48);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Create";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(307, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(307, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(221, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(396, 328);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(35, 13);
+            this.labelProgress.TabIndex = 10;
+            this.labelProgress.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelProgress;
+    }
+}
+
---
+++ KullaniciArayuzu/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+
+
+
+namespace KullaniciArayuzu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.SelectedPath;
+
+            }
+        }
+        private Bitmap GenerateQRCode(string data, int width, int height)
+        {
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            barcodeWriter.Options = new QrCodeEncodingOptions
+            {
+                Width = width,
+                Height = height
+            };
+
+            Bitmap qrCodeBitmap = barcodeWriter.Write(data);
+            return qrCodeBitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string kaynakKlasor = textBox1.Text;
+            string hedefKlasor = textBox2.Text;
+
+            int hedefGenislik = 1920;
+            int hedefYukseklik = 1500;
+
+            // Kaynak klasördeki fotoğrafları seç
+            if (string.IsNullOrWhiteSpace(kaynakKlasor) || !Directory.Exists(kaynakKlasor))
+            {
+                MessageBox.Show("Geçersiz veya boş kaynak klasörü");
+                return; // Hata durumunda işlemi sonlandır
+            }
+
+            string[] fotoDosyalar = Directory.GetFiles(kaynakKlasor);
+
+            if (fotoDosyalar.Length == 0)
+            {
+                MessageBox.Show("Kaynak klasörde fotoğraf bulunamadı.");
+                return; // Hata durumunda işlemi sonlandır
+            }
+
+            if (string.IsNullOrEmpty(hedefKlasor) || !Directory.Exists(hedefKlasor))
+            {
+                MessageBox.Show("Hedef klasör seçilmedi");
+                return; // Hata durumunda işlemi sonlandır
+            }
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += (s, ea) =>
+            {
+                for (int i = 0; i < fotoDosyalar.Length; i++)
+                {
+                    string fotoDosya = fotoDosyalar[i];
+
+                    using (Image image = Image.FromFile(fotoDosya))
+                    using (Bitmap yeniGoruntu = new Bitmap(hedefGenislik, hedefYukseklik))
+                    using (Graphics grafik = Graphics.FromImage(yeniGoruntu))
+                    {
+                        grafik.CompositingQuality = CompositingQuality.HighQuality;
+                        grafik.SmoothingMode = SmoothingMode.HighQuality;
+                        grafik.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        grafik.DrawImage(image, 0, 0, hedefGenislik, hedefYukseklik);
+                        string qrData = "QR Kod Verisi Buraya Gelecek"; // QR kodunuzun içeriğini buraya ekle
+                        Bitmap qrCodeBitmap = GenerateQRCode(qrData, 100, 100);
+
+                        // QR kodunu fotoğrafın üzerine ekler
+                        grafik.DrawImage(qrCodeBitmap, 0, 0, 100, 100);
+
+                        string yeniDosyaAdi = Path.GetFileNameWithoutExtension(fotoDosya) + "_kucuk.jpeg";
+                        yeniGoruntu.Save(Path.Combine(hedefKlasor, yeniDosyaAdi), ImageFormat.Jpeg);
+                    }
+
+                    int progressPercentage = (i + 1) * 100 / fotoDosyalar.Length;
+                    worker.ReportProgress(progressPercentage);
+                }
+            };
+
+            worker.ProgressChanged += (s, ea) =>
+            {
+                progressBar1.Value = ea.ProgressPercentage;
+                labelProgress.Text = $"İlerleme: {ea.ProgressPercentage}%"; // Yüzdeyi gösteren bir etiket 
+            };
+
+            worker.RunWorkerCompleted += (s, ea) =>
+            {
+                // İşlem tamamlandığında ProgressBarı gizle
+                progressBar1.Visible = false;
+                MessageBox.Show("FOTOĞRAFLAR BAŞARIYLA SIKIŞTIRILDI");
+            };
+
+            progressBar1.Visible = true;
+            labelProgress.Text = "İlerleme: 0%"; // Başlangıçta yüzdeyi sıfır olarak ayarla
+            worker.RunWorkerAsync();
+        }
+
+            
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        } 
+    
+    }
+    
+}
+
